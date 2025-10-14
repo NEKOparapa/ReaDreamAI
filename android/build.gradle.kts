5d38a53cd9abdc45ed852c5bd145491d372d8fcd
NEKO nyanya <40889711+NEKOparapa@users.noreply.github.com>
2025-10-14 18:21:26 +0800

    修改安卓配置

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,19 +2,24 @@
 
 pluginManagement {
     repositories {
+        // 原始配置
         google()
         mavenCentral()
         gradlePluginPortal()
+        
+        // 在这里也添加阿里云镜像
+        maven("https://maven.aliyun.com/repository/google")
+        maven("https://maven.aliyun.com/repository/public")
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
     }
 }
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        // 这里是配置项目依赖库的地方
+        // 项目依赖库的仓库配置
         google()
         mavenCentral()
-        // 你之前配置的阿里云镜像可以放在这里，用于加速依赖库下载
         maven("https://maven.aliyun.com/repository/google")
         maven("https://maven.aliyun.com/repository/public")
         maven("https://maven.aliyun.com/repository/gradle-plugin")
