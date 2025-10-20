4fc7333f13b7407ed8466765690287e0e4233d4e
NEKO nyanya <40889711+NEKOparapa@users.noreply.github.com>
2025-10-20 15:55:37 +0800

    test

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,7 +3,7 @@
 // 使用 plugins 块来声明构建脚本的插件
 plugins {
     // 定义 Android Gradle Plugin 的版本
-    id("com.android.application") version "8.2.2" apply false // 版本号可以根据你的项目调整，但 8.2.2 是一个稳定版本
+    id("com.android.application") version "8.7.0" apply false // 版本号可以根据你的项目调整，但 8.2.2 是一个稳定版本
     // 定义 Kotlin Gradle Plugin 的版本
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false // 建议与 Flutter 兼容的版本
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false // 建议与 Flutter 兼容的版本
 }
