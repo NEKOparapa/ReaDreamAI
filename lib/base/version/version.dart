da2da88f127c80dff33b07ddf6e59986aa7e11ef
NEKO nyanya <40889711+NEKOparapa@users.noreply.github.com>
2026-06-15 17:40:25 +0800

    mod: 调整版本号

--- lib/base/version/version.dart
+++ lib/base/version/version.dart
@@ -2,6 +2,6 @@
 
 class AppVersion {
   static const String appName = 'ReaDreamAI';
-  static const String version = '1.3.2';
+  static const String version = '1.3.3';
   static const String buildNumber = '1';
 }
